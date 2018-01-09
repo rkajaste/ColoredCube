f84187e6f2f4758def38fb4d90de5f2c39567fca
Richard Kajaste <rkajaste@users.noreply.github.com>
2018-01-09 20:41:14 +0200

    added project

---
+++ main.cpp
@@ -0,0 +1,279 @@
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/glew.h>
+
+#include <glfw3.h>
+GLFWwindow* window;
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+using namespace glm;
+
+#include <common/shader.hpp>
+#include <common/controls.hpp>
+
+GLfloat *changeColors(GLfloat *arr) {
+	int count = 0;
+	for (int i = 0; i < 9*12; i++) {
+		count++;
+		if (count == 1) {
+			arr[i] -= 0.01f;
+			if (arr[i] <= 0.0f) {
+				arr[i] = 1.0f;
+			}
+		}
+		else if (count > 3) {
+			count = 0;
+		}
+	}
+	return arr;
+}
+
+
+int main( void )
+{
+	// Initialise GLFW
+	if( !glfwInit() )
+	{
+		fprintf( stderr, "Failed to initialize GLFW\n" );
+		getchar();
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Open a window and create its OpenGL context
+	window = glfwCreateWindow( 1024, 768, "Colored Cube", NULL, NULL);
+	if( window == NULL ){
+		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Initialize GLEW
+	glewExperimental = true; // Needed for core profile
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
+
+	// Ensure we can capture the escape key being pressed below
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	// Dark blue background
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS); 
+
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	// Create and compile GLSL program from the shaders
+	GLuint programID = LoadShaders( "TransformVertexShader.vertexshader", "ColorFragmentShader.fragmentshader" );
+
+	// Get a handle for our "MVP" uniform
+	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+
+	// Projection matrix - 45° Field of View, 4:3 ratio, display range = 0.1 unit <-> 100 units
+	glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+
+	// Camera matrix
+	glm::mat4 View       = glm::lookAt(
+								glm::vec3(4,3,-3), // Camera is at (4,3,-3), in World Space
+								glm::vec3(0,0,0), // and looks at the origin
+								glm::vec3(0,1,0)  // Head is up
+						   );
+
+	glm::mat4 Model      = glm::mat4(1.0f);
+
+	glm::mat4 MVP        = Projection * View * Model; 
+
+	// Our vertices. Tree consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
+	// A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
+	static const GLfloat g_vertex_buffer_data[] = { 
+		-1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+
+		 1.0f, 1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f,-1.0f,
+
+		 1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f,
+		 1.0f,-1.0f,-1.0f,
+
+		 1.0f, 1.0f,-1.0f,
+		 1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f,
+
+		 1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f,
+
+		-1.0f, 1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f,
+		 1.0f,-1.0f, 1.0f,
+
+		 1.0f, 1.0f, 1.0f,
+		 1.0f,-1.0f,-1.0f,
+		 1.0f, 1.0f,-1.0f,
+
+		 1.0f,-1.0f,-1.0f,
+		 1.0f, 1.0f, 1.0f,
+		 1.0f,-1.0f, 1.0f,
+
+		 1.0f, 1.0f, 1.0f,
+		 1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f,-1.0f,
+
+		 1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+
+		 1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+		 1.0f,-1.0f, 1.0f
+	};
+
+	// One color for each vertex
+	static GLfloat g_color_buffer_data[] = { 
+		0.0f,  0.0f,  0.0f,
+		0.0f,  0.0f,  0.0f,
+		0.0f,  0.0f,  0.0f,
+
+		0.0f,  0.0f,  0.0f,
+		0.0f,  0.0f,  0.0f,
+		0.0f,  0.0f,  0.0f,
+
+		0.0f,  0.0f,  0.0f,
+		0.0f,  0.0f,  0.0f,
+		0.0f,  0.0f,  0.0f,
+		
+		0.0f,  0.0f,  0.0f,
+		0.0f,  0.0f,  0.0f,
+		0.0f,  0.0f,  0.0f,
+
+		0.0f,  0.0f,  0.0f,
+		0.0f,  0.0f,  0.0f,
+		0.0f,  0.0f,  0.0f,
+
+		0.0f,  0.0f,  0.0f,
+		0.0f,  0.0f,  0.0f,
+		0.0f,  0.0f,  0.0f,
+
+		0.0f,  0.0f,  0.0f,
+		0.0f,  0.0f,  0.0f,
+		0.0f,  0.0f,  0.0f,
+
+		0.0f,  0.0f,  0.0f,
+		0.0f,  0.0f,  0.0f,
+		0.0f,  0.0f,  0.0f,
+
+		0.0f,  0.0f,  0.0f,
+		0.0f,  0.0f,  0.0f,
+		0.0f,  0.0f,  0.0f,
+
+		0.0f,  0.0f,  0.0f,
+		0.0f,  0.0f,  0.0f,
+		0.0f,  0.0f,  0.0f,
+
+		0.0f,  0.0f,  0.0f,
+		0.0f,  0.0f,  0.0f,
+		0.0f,  0.0f,  0.0f,
+
+		0.0f,  0.0f,  0.0f,
+		0.0f,  0.0f,  0.0f,
+		0.0f,  0.0f,  0.0f
+	};
+
+	GLuint vertexbuffer;
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+	GLuint colorbuffer;
+	glGenBuffers(1, &colorbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+
+	do{
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glUseProgram(programID);
+
+
+		computeMatricesFromInputs();
+		glm::mat4 ProjectionMatrix = getProjectionMatrix();
+		glm::mat4 ViewMatrix = getViewMatrix();
+		glm::mat4 ModelMatrix = glm::mat4(1.0);
+		glm::mat4 MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
+
+		// Send transformation to the currently bound shader
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(
+			0,                  
+			3,                  
+			GL_FLOAT,           
+			GL_FALSE,           
+			0,                  
+			(void*)0            
+		);
+
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), changeColors(g_color_buffer_data), GL_STATIC_DRAW);
+		glVertexAttribPointer(
+			1,                          
+			3,                             
+			GL_FLOAT,                       
+			GL_FALSE,                        
+			0,                               
+			(void*)0                          
+		);
+
+		// Draw the triangles
+		glDrawArrays(GL_TRIANGLES, 0, 12*3); // 12*3 indices starting at 0 -> 12 triangles
+
+		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
+
+		// Swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	} 
+
+	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+		   glfwWindowShouldClose(window) == 0 );
+
+	// Cleanup VBO and shader
+	glDeleteBuffers(1, &vertexbuffer);
+	glDeleteBuffers(1, &colorbuffer);
+	glDeleteProgram(programID);
+	glDeleteVertexArrays(1, &VertexArrayID);
+
+	glfwTerminate();
+
+	return 0;
+}
+
---
+++ src/common/controls.cpp
@@ -0,0 +1,87 @@
+#include <glfw3.h>
+extern GLFWwindow* window;
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+using namespace glm;
+
+#include "controls.h"
+
+glm::mat4 ViewMatrix;
+glm::mat4 ProjectionMatrix;
+
+glm::mat4 getViewMatrix() {
+	return ViewMatrix;
+}
+glm::mat4 getProjectionMatrix() {
+	return ProjectionMatrix;
+}
+
+glm::vec3 position = glm::vec3(0, 0, 5);
+
+float horizontalAngle = 3.14f;
+
+float verticalAngle = 0.0f;
+
+float initialFoV = 45.0f;
+
+float speed = 3.0f; 
+float mouseSpeed = 0.002f;
+
+void computeMatricesFromInputs() {
+
+	static double lastTime = glfwGetTime();
+
+	double currentTime = glfwGetTime();
+	float deltaTime = float(currentTime - lastTime);
+
+	double xpos, ypos;
+	glfwGetCursorPos(window, &xpos, &ypos);
+	// Reset cursor for next frame
+	glfwSetCursorPos(window, 1024 / 2, 768 / 2);
+
+	horizontalAngle += mouseSpeed * float(1024 / 2 - xpos);
+	verticalAngle += mouseSpeed * float(768 / 2 - ypos);
+
+	glm::vec3 direction(
+		cos(verticalAngle) * sin(horizontalAngle), //x
+		sin(verticalAngle),						   //y
+		cos(verticalAngle) * cos(horizontalAngle)  //z
+	);
+
+	glm::vec3 right = glm::vec3(
+		sin(horizontalAngle - 3.14f / 2.0f),
+		0,
+		cos(horizontalAngle - 3.14f / 2.0f)
+	);
+
+	glm::vec3 up = glm::cross(right, direction);
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+		position += direction * deltaTime * speed;
+	}
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+		position -= direction * deltaTime * speed;
+	}
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+		position += right * deltaTime * speed;
+	}
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+		position -= right * deltaTime * speed;
+	}
+	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+		position += up * deltaTime * speed;
+	}
+
+	float FoV = initialFoV;
+
+	ProjectionMatrix = glm::perspective(glm::radians(FoV), 4.0f / 3.0f, 0.1f, 100.0f);
+
+	ViewMatrix = glm::lookAt(
+		position,           
+		position + direction,
+		up                  
+	);
+
+	lastTime = currentTime;
+}
